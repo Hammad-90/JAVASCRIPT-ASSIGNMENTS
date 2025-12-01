9c2bbad70e4fea1865a2d6d3c4a14ee26f37cf2d
M.Hammad <mh4009577@gmail.com>
2025-12-02 02:33:55 +0500

    Add files via upload

---
+++ java/app.js
@@ -0,0 +1,133 @@
+   // (---------------------------CHAPTER 01----------------------------)
+
+//    JAVASCRIPT 
+
+//         Q1
+// alert("Welcome")
+
+//          Q2 
+// alert("Error! Please enter a valid password")
+
+ //          Q3
+ // alert("Welcome to Js Land... \n Happy Coding")
+
+//           Q4
+//   alert("welcome to js Land");
+//   alert("Happy Coding")   
+
+//           Q5
+//  console.log(alert("Hello I can run Js through my web browswer console")) 
+
+//           Q6 
+// alert("I am Using alert in my Assignment of JavaScript")
+
+//           Q7 
+// alert("for Answer Q7 of Chapter ! go to index.html")
+
+
+//   (-------------------------CHAPTER  02----------------------------)
+
+//           Q1 
+var userName;
+
+//           Q2 
+var myName ="M.HAMMAD";
+
+var firstName = "M.HAMMAD ";
+var lastName = "AKM";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+//           Q3 
+// var message;
+// message ="Hello World!";
+// alert(message);
+
+//           Q4 
+var name="M.HAMMAD";
+var age="17";
+var certification ="WEB And APP Development"
+
+//  alert(name);
+//  alert(age);
+//  alert(certification);
+
+//           Q5 
+// alert("PIZZA");
+// alert("PIZZ");
+// alert("PIZ");
+// alert("PI");
+// alert("P");
+
+//           Q6 
+var MyEmail = "xyz@gmail.com";
+// alert ("My Email Address is : " + MyEmail);
+
+//           Q7 
+var Book ="A smarter way to learn JS";
+// alert("I am trying to learn from the Book : "+  Book);
+
+//           Q8 
+// document.write("Yah1 I can write HTML content through JavaScript");
+
+//           Q9
+// alert("STAR");
+
+// (----------------------------CAPTER  03-------------------------------)
+
+//chapter 3 ( Q1)
+
+ var myAge = 17;
+//  alert("I am " + myage + " year old")
+
+//chapter 3 ( Q2)
+
+var pageVistedCount =20;
+// alert("you visited this website " + pageVistedCount +" time!");
+
+//chapter 3 ( Q3)
+
+var birthYear ;
+birthYear = 2009;
+// alert("My birth Year is : "birthYear);
+
+//chapter 3 ( Q4)
+
+var user_Name = "HAMMAD";
+var productName = "WATCHES";
+var quantity = 7;
+ 
+// console.log(user_Name + " order " + quantity + " " + productName + " from WATCH store");
+
+// (--------------------------CHAPTER 4--------------------------------)
+
+//             Q1
+
+var  name01="HAMMAD" ; var num= 17; var learning = "JavaScript";
+
+//             Q2
+
+// ---legal---
+ var user_name;
+ var $user_name;
+ var username;
+ var userName;
+ var user_name;
+ var userName01
+
+ //---illigal---
+
+ var 1username; //starting with a number
+
+ var USERNAME ="AHMED"; // case sensitive
+ var username /// this not same as  USERNAME
+ console.log(USERNAME);
+ console.log(username)
+
+ var user name = "HAMMAD" // space between numbers
+
+ var user@Name ="someone" // using any symbol (excluded $  and  _ )
+
+ var var ="value";  //using js keyword or method as variables;
+
+
