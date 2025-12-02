764dc059fe223f42226521f827a08986b4648dd8
M.Hammad <mh4009577@gmail.com>
2025-12-03 01:44:07 +0500

    Add files via upload

---
+++ JS 02/app.js
@@ -0,0 +1,291 @@
+// -------------------CHAPTER 05 (MATH EXPRESSIONS)-------------------
+
+//                QUESTION (1)
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 + num2;
+// console.log("Two values are: " + num1 + " and "+  num2)
+// console.log("The sum of numbers is : "+total);
+
+//                 QUESTION (2)
+
+// console.log("Two values are: " + num1 + " and "+  num2)
+
+// var total = num1 * num2;
+// console.log("The Muiltiplication of two number is: "+total);
+// var total = num1 / num2;
+// console.log("The Division of two number is: "+total);
+// var total = num1 - num2;
+// console.log("Subtraction of two number is: "+total);
+// var total = num1 % num2;
+// console.log("Modules of two number is: "+total);
+
+
+//                 QUESTION (3)
+
+// var myNumber ;
+// console.log("value after declaration :" + myNumber);
+// var myNumber = 3 + 2;
+// console.log("initial Value : "+ myNumber);
+
+// myNumber++;
+// console.log("Value after increment : " + myNumber)
+
+//  myNumber = myNumber + 7;
+
+//  console.log("value after adding 7 is: " + myNumber)
+
+//  myNumber--;
+
+//  console.log("value after decrement is : "+ myNumber)
+
+//  var remainder = myNumber % 3;
+  
+//  console.log("final Remainder of value is: "+ remainder)
+
+
+//                   QUESTION (4)
+
+// var ticketPrice = 600;
+// var ticketPurchased = 5;
+
+// var totalAmount =  ticketPrice * ticketPurchased;
+
+// console.log("Total cost to buy "+ ticketPurchased + " tickets to a movie is " + totalAmount + " PKR.")
+
+
+//                   QUESTION (5)
+
+// var tableNumber =4;
+
+// console.log("Table of " + tableNumber + " is following:" )
+// console.log(tableNumber + " X  1 = " + (tableNumber*1));
+// console.log(tableNumber + " X  2 = " + (tableNumber*2));
+// console.log(tableNumber + " X  3 = " + (tableNumber*3));
+// console.log(tableNumber + " X  4 = " + (tableNumber*4));
+// console.log(tableNumber + " X  5 = " + (tableNumber*5));
+// console.log(tableNumber + " X  6 = " + (tableNumber*6));
+// console.log(tableNumber + " X  7 = " + (tableNumber*7));
+// console.log(tableNumber + " X  8 = " + (tableNumber*8));
+// console.log(tableNumber + " X  9 = " + (tableNumber*9));
+// console.log(tableNumber + " X  10 = " + (tableNumber*10));
+
+
+//                    QUESTION (6)
+
+//           ---Temperature Converter---
+
+//           -- converted to fahrenhite --
+// var celsius = 40;
+// console.log("Converting Temperature  from ceslius (40deg) to fahrenhite");
+// fahrenhite = (celsius*9/5)+32;
+// console.log("40 degree celsius is equal to : " + fahrenhite + " fahrenhite")
+
+//             ---converted to celsius---
+// fahrenhite = 104;
+// console.log("converting " + fahrenhite  + " fahrenhite  to degree celsius!")
+// var celsius = (fahrenhite-32)*5/9;
+// console.log("converted 104 fahrenhite to celsius is: " + celsius )
+
+
+//                 ---QUESTION (7)---
+
+// var item1Price = 650;
+// var item2Price = 100;
+// var item1Quantity =3;
+// var item2Quantity =7;
+// var shipmentCharge =100;
+
+// console.log("Price of item one : " + item1Price)
+// console.log("Quantity of item one : " + item1Quantity)
+// console.log("Price of item two : " + item2Price)
+// console.log("Quantity of item two : " + item2Quantity)
+// console.log("Shipping Charges  : " + shipmentCharge)
+// var totalCost = (item1Price*item1Quantity) + (item2Price*item2Quantity) + shipmentCharge;
+// console.log("Total cost fo your order is : " + totalCost)
+
+
+//                    QUESTION (8)
+
+//                   --MARK SHEET--
+
+// var obtained_mark = 804;
+// var totalMark=980;
+// console.log("Total Number : " +  totalMark);
+// console.log("Obtaining Mark : " + obtained_mark)
+// var percentage = (obtained_mark/totalMark)*100;
+// console.log("Percentage of marks is: " + percentage)
+
+
+//                    QUESTION (9)
+
+// var usDollerRate = 104.80;
+// var usDollerCount = 10;
+// var saudiRiyalRate = 28;
+// var saudiRiyalCount=25;
+
+// var usCash = (usDollerRate*usDollerCount);
+// var saudiCash = (saudiRiyalRate*saudiRiyalCount);
+// console.log("Total Us Dollers : "+ usCash);
+// console.log("Total Saudi Riyal Converted : " + saudiCash)
+// console.log("Totol Cash Convertion " + (usCash + saudiCash) );
+
+
+
+//                  QUESTION (10)
+
+// var  arithmetic_caluculation= (2+3) * 10 / 2;
+// console.log("arithmetic_caluculation= (2+3) * 10 / 2;");
+// console.log("Answer is : " + arithmetic_caluculation );
+
+
+//                  QUESTION (11) 
+
+//                (Age-Calculater)
+
+// var currentYear =2016;
+// var birthYear = 1992;
+// console.log("Current Year : " + currentYear);
+// console.log("Birth Year : " + birthYear)
+// var exactAge= (currentYear-birthYear);
+// console.log(exactAge);
+
+//                  QUESTION (12)
+
+
+//               ---The Geometrizer---
+
+// var radius =20;
+// var circumference =  (20*2)*3.142;
+// console.log(circumference)
+
+
+//                  QUESTION (13)
+
+// var snack ="Cheetos";
+// var ageNow = 22;
+// var endAge = 100;
+// var perDay = 2;
+
+// console.log("current Age :" + ageNow)
+// console.log("End year :" + endAge)
+
+// var remainignAge = (endAge-ageNow);
+// console.log("Remaining Age Left : " +remainignAge)
+
+// var yearToDays = (remainignAge*365.25);
+// var  neededRes = (yearToDays*2);
+// console.log("Per day Consumption :" + perDay)
+// console.log("Needed Resourses in  Remaining Years in total : "+ neededRes)
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+//                       CHAPTER (6-TO-9)
+
+
+//                        QUESTION (1)
+
+// var N = 10;
+// console.log("The value of N is : " + N )
+// // --------------------------------------------
+// console.log("The valus of ++N is: " +  ++N)
+// console.log("Now the Value is: " + N)
+// // ---------------------------------------------
+// console.log("The valus of ++N is: " +  N++)
+// console.log("Now the Value is: " + N)
+// // --------------------------------------------
+// console.log("The valus of ++N is: " +  --N)
+// console.log("Now the Value is: " + N)
+// // ---------------------------------------------
+// console.log("The valus of ++N is: " +  N--)
+// console.log("Now the Value is: " + N)
+
+
+//                  QUESTION (2)
+
+//  var a = 2;
+//  var b = 1;
+
+//  console.log("A = 2 ;  B = 1");
+
+//  console.log("A is : : " + a)
+//  console.log("B is : " + b)
+//  var result = --a - --b + ++b +b--;
+//  console.log("Result is : " + result )
+
+//                  QUESTION (3)
+
+// var output ;
+// var userName =prompt("Please enter you Name!");
+// if(!userName || userName === "")
+// {
+//     alert("Please Enter a Valid Name: ")
+//     userName =prompt("Please Enter your Name Again!")
+//    var output  ="Hi " + userName  + " \nhope you are fine!";
+//    alert(output); 
+// }
+
+// else 
+// {
+// var output  ="Hi " + userName  + " \nhope you are fine!";
+// alert(output); 
+// }
+
+//                  QUESTION (4-5)
+
+// var defaultTable =5;
+// var tableNumber =Number(prompt("Enter a Table Number!"));
+// var userInput = tableNumber;
+// if(!userInput)
+// {
+//     console.log("Table of " + defaultTable + " is following:" )
+// console.log(defaultTable + " X  1 = " + (defaultTable*1));
+// console.log(defaultTable + " X  2 = " + (defaultTable*2));
+// console.log(defaultTable + " X  3 = " + (defaultTable*3));
+// console.log(defaultTable + " X  4 = " + (defaultTable*4));
+// console.log(defaultTable + " X  5 = " + (defaultTable*5));
+// console.log(defaultTable + " X  6 = " + (defaultTable*6));
+// console.log(defaultTable + " X  7 = " + (defaultTable*7));
+// console.log(defaultTable + " X  8 = " + (defaultTable*8));
+// console.log(defaultTable + " X  9 = " + (defaultTable*9));
+// console.log(defaultTable + " X  10 = " + (defaultTable*10));
+// }
+// else {
+
+// console.log("Table of " + tableNumber + " is following:" )
+// console.log(tableNumber + " X  1 = " + (tableNumber*1));
+// console.log(tableNumber + " X  2 = " + (tableNumber*2));
+// console.log(tableNumber + " X  3 = " + (tableNumber*3));
+// console.log(tableNumber + " X  4 = " + (tableNumber*4));
+// console.log(tableNumber + " X  5 = " + (tableNumber*5));
+// console.log(tableNumber + " X  6 = " + (tableNumber*6));
+// console.log(tableNumber + " X  7 = " + (tableNumber*7));
+// console.log(tableNumber + " X  8 = " + (tableNumber*8));
+// console.log(tableNumber + " X  9 = " + (tableNumber*9));
+// console.log(tableNumber + " X  10 = " + (tableNumber*10));
+// }
+
+//                     QUESTION (6)
+
+// var subject1 = prompt("Enter your First Subject Name!")
+// var sub01_Obtain_Mark =Number(prompt("Enter "+ subject1 +" subject Obtained Mark"));
+// var subject2 = prompt("Enter your Second Subject Name!")
+// var sub02_Obtain_Mark =Number(prompt("Enter  "+ subject2 +" subject  Obtained Mark"));
+// var subject3 = prompt("Enter your Third Subject Name!")
+// var sub03_Obtain_Mark =Number(prompt("Enter  "+ subject3 +" subject  Obtained Mark"));
+
+// var Total_Marks_Per_Subject =100;
+
+// console.log("Total Subjects and its Marks percentage!")
+// console.log(subject1 + " " + Total_Marks_Per_Subject + " " +((sub01_Obtain_Mark/Total_Marks_Per_Subject)*100) + "%")
+// console.log(subject2 + " " + Total_Marks_Per_Subject + " " + ((sub02_Obtain_Mark/Total_Marks_Per_Subject)*100) + "%" )
+// console.log(subject3 + " " + Total_Marks_Per_Subject + " " + ((sub03_Obtain_Mark/Total_Marks_Per_Subject)*100)+ "%")
+
+
+//  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                                   END
+//  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
